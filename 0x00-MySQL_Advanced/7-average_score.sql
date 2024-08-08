5a5aaa0583e0bb18cc61bc1667c8fea7e47102b6
RudzaniMatidze <rdzmatidze@gmail.com>
2024-08-08 01:13:40 +0000

    file updated

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,22 +2,27 @@
 -- computes and stores the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
+
+CREATE PROCEDURE ComputeAverageScoreForUser (IN in_user_id INT)
 BEGIN
     DECLARE total_score INT DEFAULT 0;
     DECLARE projects_count INT DEFAULT 0;
 
-    SELECT SUM(score)
-        INTO total_score
-	FROM corrections
-	WHERE corrections.user_id = user_id;
-    SELECT COUNT(*)
-        INTO projects_count
-	FROM corrections
-	WHERE corrections.user_id = user_id;
+    -- Calculate the total score for the user
+    SELECT IFNULL(SUM(score), 0)
+    INTO total_score
+    FROM corrections
+    WHERE corrections.user_id = in_user_id;
 
+    -- Count the number of projects for the user
+    SELECT COUNT(*)
+    INTO projects_count
+    FROM corrections
+    WHERE corrections.user_id = in_user_id;
+
+    -- Update the user's average score
     UPDATE users
-        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
-	WHERE user_id = user_id;
+    SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+    WHERE users.id = in_user_id;
 END $$
 DELIMITER ;
