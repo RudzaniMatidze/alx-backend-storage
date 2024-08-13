94076bf0e9eb7d7fcc99647ede378da46b283109
RudzaniMatidze <rdzmatidze@gmail.com>
2024-08-13 06:05:55 -0700

    function that returns all students sorted by average score:

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+ function that returns all students sorted by average score
+"""
+import pymongo
+
+
+def top_students(mongo_collection):
+    """prints all students sorted by average score"""
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
